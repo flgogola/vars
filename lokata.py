2db9beafa59c67695099334aeada91f0e8c495dc
flgogola <florentynagogola@gmail.com>
2023-10-22 16:54:47 +0200

    Initial commit

---
+++ lokata.py
@@ -0,0 +1,9 @@
+inwestycja_początkowa = 46_567.
+oprocentowanie_roczne = 0.075
+a = 3
+a *= 1
+
+
+
+print("Zysk z", str(a), "letniej lokaty bankowej o wartości początkowej", str(inwestycja_początkowa) +" zł" + ", " + "oprocentowaniu rocznym", str(oprocentowanie_roczne*100)+"%", "oraz corocznej kapitalizacji odsetek wynosi", str(inwestycja_początkowa * ((1+ str(oprocentowanie_roczne)) * a) + " zł.))
+                                                                                                                                                                                                                                      
